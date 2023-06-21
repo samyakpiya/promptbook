6756b6eda184b649ca34acf4fa0b493424948cb7
Samyak Piya <samyakpiya@gmail.com>
2023-06-21 12:36:09 -0400

    Implement Cache-Control on retreived prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,10 @@ export const GET = async (req) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: { "Cache-Control": "no-store" },
+    });
   } catch (error) {
     return new Response("Failed to fetch all prompts!", { status: 500 });
   }
